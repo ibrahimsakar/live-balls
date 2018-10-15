069c662eea1b5fe41d02e82497553a2aa149b942
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-10-15 21:38:54 +0300

    indexFactory added.

---
+++ public/javascripts/controllers/indexController.js
@@ -0,0 +1,15 @@
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+
+    const connectionOptions = {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 600
+    };
+    
+    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+    .then((socket) => {
+        console.log('Bağlantı gerçekleşti', socket);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}])
