affc7cfcd50040efc9a5eee3cd13f52660ed0c47
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-10-15 22:07:38 +0300

    User connected info with newUser emit

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,5 +1,7 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
+    $scope.messages = [];
+
     $scope.init = () => {
         const username = prompt('Please  enter username');
         
@@ -18,6 +20,17 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
         indexFactory.connectSocket('http://localhost:3000', connectionOptions)
         .then((socket) => {
             socket.emit('newUser', {username});
+
+            socket.on('newUser', (data) => {
+                const messageData = {
+                    type: 0,
+                    username: data.username
+                };
+
+                $scope.messages.push(messageData);
+
+                $scope.$apply();
+            });
         })
         .catch((err) => {
             console.log(err);
