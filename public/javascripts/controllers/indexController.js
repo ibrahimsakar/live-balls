7e351f0443980481e2da85baf5b38bccd0544c6c
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-11-18 23:46:11 +0300

    Player animations added.

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -52,6 +52,17 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
                 $scope.messages.push(messageData);
                 $scope.$apply();
             });
+
+            let animate = false;
+            $scope.onClickPlayer = ($event) => {
+                if(!animate){
+                    animate = true;
+                    $('#' + socket.id).animate({'left': $event.offsetX, 'top': $event.offsetY}, () => {
+                        animate = false;
+                    });
+                }
+            };
+
         })
         .catch((err) => {
             console.log(err);
